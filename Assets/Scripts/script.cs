7b6830b413d7ba952b8807ddb43cf04a1673c888
Dmitriy Kulyasov <vvvvvvmaz@gmail.com>
2022-05-22 09:46:04 +0300

    1 commit

---
+++ Assets/Scripts/script.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    public SpriteRenderer Target;
+
+
+    void Update()
+    {
+        
+    }
+}
